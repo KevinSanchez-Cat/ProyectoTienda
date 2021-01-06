a71c5c5649405d79eec2ee243876668439418d58
Kevin Ivan Sanchez Valdin <62733314+KevinSanchez-Cat@users.noreply.github.com>
2021-01-06 06:55:04 -0600

    Busqueda de productos

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -19,6 +19,41 @@ namespace ProyectoTienda.Controllers
         }
 
         [HttpPost]
+        public ActionResult BuscaProd(string nomBuscar)
+        {
+            ViewBag.SearchKey = nomBuscar;
+            using (db)
+            {
+                var query = from st in db.Producto
+                            where st.nombre_producto.Contains(nomBuscar)
+
+                            select st;
+
+                var listProd = query.ToList();
+                ViewBag.Listado = listProd;
+            }
+
+            return View();
+        }
+        public ActionResult prodCategoria(int idCat)
+        {
+            List<Producto> mercancia = null;
+            var query = from p in db.Producto
+                        where p.id_categoria == idCat
+                        select p;
+            if (idCat == 1)
+            {
+                List<Producto> motoresE1 = query.ToList();
+                mercancia = motoresE1;
+                ViewBag.cat = "Motores linea 1";
+            }
+            if (idCat == 2)
+            {
+                List<Producto> motoresE2 = query.ToList();
+                mercancia = motoresE2;
+                ViewBag.cat = "Motores linea 2";
+            }
+        }
 
     }
 }
