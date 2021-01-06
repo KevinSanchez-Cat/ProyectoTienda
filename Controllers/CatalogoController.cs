b2d7f282b8a82c7da67ee8cf50033b642c634354
Kevin Ivan Sanchez Valdin <62733314+KevinSanchez-Cat@users.noreply.github.com>
2021-01-06 06:21:57 -0600

    Creacion de layouts

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyectoTienda.Models;
+
 
 namespace ProyectoTienda.Controllers
 {
+
     public class CatalogoController : Controller
     {
+        private ContextTienda db = new ContextTienda();
         // GET: Catalogo
         public ActionResult Catalogo()
         {
             return View();
         }
+
+        [HttpPost]
+
     }
 }
